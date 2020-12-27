9d214f28bb7f176d1f20771280c6d1cdc7035c21
mchez808 <mark.chesney@gmail.com>
2020-12-27 10:33:06 -0800

    add class attribute

--- animals/dogs.py
+++ animals/dogs.py
@@ -1,7 +1,15 @@
 class Dog(object):
-    """docstring for Dog."""
+    """Dog class to demonstrate OOP concepts: inheritance"""
+
+    # Class attribute
+    species = "Canis familiaris"
 
     def __init__(self, name, age):
         super(Dog, self).__init__()
         self.name = name
         self.age = age
+
+
+if __name__ == '__main__':
+    lee = Dog(name='Leeloo', age=8)
+    zel = Dog(name='Zelda', age=11)
