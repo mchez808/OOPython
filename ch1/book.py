b83b6bb9c7814b186fac53600d2196dfbacdb232
KMQC <kmqc@novonordisk.com>
2021-01-07 12:40:09 -0800

    add cond logic for _discount (internal use attr)

--- ch1/book.py
+++ ch1/book.py
@@ -11,7 +11,10 @@ class Book(object):
     
     def get_price(self):
         """return price of book. instance method (not class method)"""
-        return self.price
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
     
     def set_discount(self, amount):
         """modifies discount on book object. (A mutator)"""
@@ -24,4 +27,8 @@ class Book(object):
 if __name__ == "__main__":
     b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
     b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+    
     print(b1.get_price())
+    b2.set_discount(0.10)
+    print(b2.get_price())
+
