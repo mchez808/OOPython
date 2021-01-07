9d69643fb3a417cb91c7c9ff5fa4c29aae9da161
KMQC <kmqc@novonordisk.com>
2021-01-07 12:12:41 -0800

    add ch1 Book

---
+++ ch1/book.py
@@ -0,0 +1,12 @@
+class Book(object):
+    """
+    docstring
+    """
+    def __init__(self, title):
+        """
+        docstring
+        """
+        self.title = title
+
+bk1 = Book("Life of Pi")
+bk2 = Book("The Art of War")
