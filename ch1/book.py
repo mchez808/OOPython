c8dd97d7382d5ebc537ac3de2eacc98c0d4e1342
KMQC <kmqc@novonordisk.com>
2021-01-07 12:20:42 -0800

    add docstrings

--- ch1/book.py
+++ ch1/book.py
@@ -1,12 +1,12 @@
 class Book(object):
     """
-    docstring
+    [from PEP 257] The docstring for a class should summarize its behavior and list the public methods and instance variables. If the class is intended to be subclassed, and has an additional interface for subclasses, this interface should be listed separately (in the docstring). The class constructor should be documented in the docstring for its __init__ method. Individual methods should be documented by their own docstring.
     """
     def __init__(self, title):
-        """
-        docstring
-        """
+        """Inits Book with parameters"""
         self.title = title
 
-bk1 = Book("Life of Pi")
-bk2 = Book("The Art of War")
+
+if __name__ == "__main__":
+    bk1 = Book("Life of Pi")
+    bk2 = Book("The Art of War")
