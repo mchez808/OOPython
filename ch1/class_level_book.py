ed743cc52b401bab4a7f259d79db1160d723da48
KMQC <kmqc@novonordisk.com>
2021-01-07 18:02:51 -0800

    add booktype check

--- ch1/class_level_book.py
+++ ch1/class_level_book.py
@@ -1,6 +1,7 @@
 # class methods and attributes
 class Book:
     # TODO: properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: dunder properties are hidden from other classes
 
@@ -13,7 +14,13 @@ class Book:
     def set_title(self, new_title):
         self.title = new_title
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
-    
-    
+        if booktype.upper() not in Book.BOOK_TYPES:
+            raise TypeError
+        self.booktype = booktype
+
+
+if __name__ == "__main__":
+    b1 = Book("The Giver", "eBook")
+    b2 = Book("The Alchemist", "Post-it notes")
