18cd173913fb4ec004cbe755c21081e1fb14ba68
KMQC <kmqc@novonordisk.com>
2021-01-07 19:55:25 -0800

    add staticmethod and singleton example

--- ch1/class_level_book.py
+++ ch1/class_level_book.py
@@ -4,16 +4,7 @@ class Book:
     BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: dunder properties are hidden from other classes
-
-    # TODO: create a class method
-    @classmethod
-    def get_book_types(cls):
-        """Returns the BOOK_TYPES constant.
-        operates on a class instance, not an object instance.
-        """
-        return cls.BOOK_TYPES
-
-    # TODO: create a static method
+    __booklist = None   # essentially a private variable
 
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
@@ -27,6 +18,25 @@ class Book:
         else:
             self.booktype = booktype
 
+    # TODO: create a class method
+    @classmethod
+    def get_book_types(cls):
+        """Returns the BOOK_TYPES constant.
+        operates on a class instance, not an object instance.
+        """
+        return cls.BOOK_TYPES
+
+    # TODO: create a static method
+    @staticmethod
+    def get_book_list(cls):
+        if Book.__booklist is None:
+            Book.__booklist = []
+        return Book.__booklist
+
+    # by definition, it neither modifies the state of the class nor of objects
+    # useful when it makes sense for the method to belong to the class,
+    # i.e. good way of namespacing certain kinds of methods.
+
 
 if __name__ == "__main__":
     b1 = Book("The Giver", "eBook")
@@ -34,6 +44,13 @@ if __name__ == "__main__":
         b2 = Book("The Alchemist", "Post-it notes")
     except ValueError as e:
         print("Expected fail case: success")
+        b2 = Book("The Alchemist", "paperback")
 
     # access the class attribute through class method
     print("Book types:", Book.get_book_types())
+
+    # use the static method to access a singleton object
+    books = Book.get_book_list(Book)
+    books.append(b1)
+    books.append(b2)
+    print(books)
