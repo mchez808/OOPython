cd40b8c6609afa69ac0223ea4db0f596336148a3
KMQC <kmqc@novonordisk.com>
2021-01-07 17:10:01 -0800

    add isinstance examples

--- ch1/types.py
+++ ch1/types.py
@@ -28,3 +28,13 @@ if __name__ == "__main__":
     print("types equal?", type(b1) == type(b2))
     print("is equal?", b1 is b2)
     print("are values equal?", b1 == b2)
+
+    input("cleaner output: isinstance...")
+
+    print(isinstance(b1, Book))
+    print(isinstance(f1, Film))
+
+    # every class is a subclass of the object class
+    # (All Python's built-in types have lower case: object, int, str, unicode, float, bool, etc.)
+    if not isinstance(b1, object):
+        raise TypeError
