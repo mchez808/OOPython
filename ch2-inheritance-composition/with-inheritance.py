4cefa618acf03b9a4c23118c72effaa328bb3e47
KMQC <kmqc@novonordisk.com>
2021-01-10 12:24:50 -0800

    add Magazine sub-class

--- ch2-inheritance-composition/with-inheritance.py
+++ ch2-inheritance-composition/with-inheritance.py
@@ -12,6 +12,16 @@ class Book(Publication):
         self.pages = pages
 
 
+class Magazine(Publication):
+    def __init__(self, title, price, period, publisher):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+
 if __name__ == "__main__":
     b1 = Book("Brave New World", 29.0, "Aldous Huxley", 311)
     print(b1.title, b1.price, b1.author, b1.pages, sep=', ')
+
+    m1 = Magazine("Scientific American", 5.99, "Monthly", "Springer Nature")
+    print(m1.title, m1.price, m1.period, m1.publisher, sep=', ')
