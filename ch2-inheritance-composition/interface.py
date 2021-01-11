0867e7ebc025ce9b34aa78fd594e425ddc260000
KMQC <kmqc@novonordisk.com>
2021-01-11 14:42:28 -0800

    implement Circle subclass

--- ch2-inheritance-composition/interface.py
+++ ch2-inheritance-composition/interface.py
@@ -2,12 +2,26 @@
 # Python doesn't have explicit language support for this as a built-in part of the language.
 # An interface is a contract a particular class is making to provide certain behavior.
 from abc import ABC, abstractmethod
-
+from math import pi
 
 class GraphicShape(ABC):
+    """
+    abstract base class, with @abstractmethod
+    """
     def __init__(self):
         super().__init__()
 
     @abstractmethod
     def calcArea(self):
         pass
+
+
+class Circle(GraphicShape):
+    """
+    Circle class, inheriting from abstract base class
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calcArea(self):
+        return pi * self.radius ** 2
