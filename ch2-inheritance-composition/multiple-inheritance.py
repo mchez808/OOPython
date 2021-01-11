37e07db2ffa194773fbfc72178a2ac696d8fd69c
KMQC <kmqc@novonordisk.com>
2021-01-11 12:43:33 -0800

    add C.__mro__ and MRO notes

--- ch2-inheritance-composition/multiple-inheritance.py
+++ ch2-inheritance-composition/multiple-inheritance.py
@@ -1,11 +1,16 @@
 # Unlike other languages, Python lets you define classes that can inherit from more than one base class. 
 # multiple inheritance: while it can be a useful tool, it can also cause a lot of problems if you don't use it carefully.
 
-# For self.name, MRO starts in current class, 
+# For self.name, the Method Resolution Order (MRO) starts in current class, 
 # in this case, in class C, which has no definition of name.
 # Python then goes to the superclasses of the line (after ~20):
 # 
 #    C(A, B)        in order, from left to right.
+#    
+#    D(B, A)        <-- this would alter the MRO.
+
+# this added complexity is one of the main reasons why you don't see a whole lot of multiple inheritance in real world projects, 
+# but there is a place where they actually are very useful and that is in implementing a programming construct called an interface.
 
 class A:
     def __init__(self):
@@ -33,8 +38,24 @@ class C(A, B):  # both A and B defined as base classes
         print(self.name)
 
 
+class D(B, A):
+    def __init__(self):
+        super().__init__()
+
+    def show_name(self):
+        print(self.name)
+    
+
 if __name__ == "__main__":
     c = C()
     c.show_attr()   # non-conflicting attributes print just fine
 
-    c.show_name()   # conflict?  Method Resolution Order: AAA prints out.
+    c.show_name()   # conflict?  MRO: AAA prints out.
+
+    # new MRO
+    d = D()
+    d.show_name()
+
+    # print out MRO's
+    print(C.__mro__)
+    print(D.__mro__)
