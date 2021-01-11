3563cd9d79060a02dc5e582a7bcd8ffd744f4fa9
KMQC <kmqc@novonordisk.com>
2021-01-11 14:32:08 -0800

    add imports: ABC, abstractmethod

--- ch2-inheritance-composition/interface.py
+++ ch2-inheritance-composition/interface.py
@@ -1,3 +1,5 @@
 # A combination of multiple inheritance and abstract base classes can be used to implement an interface.
 # Python doesn't have explicit language support for this as a built-in part of the language.
 # An interface is a contract a particular class is making to provide certain behavior.
+from abc import ABC, abstractmethod
+
