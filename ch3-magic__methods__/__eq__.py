50cdba1f76e9b16119a2f3a7d5d9a31250429c96
Mark Chesney <mark.chesney@gmail.com>
2021-01-16 15:39:49 -0800

    Linkedin learning (#4)
    * add interface.py
    
    * add imports: ABC, abstractmethod
    
    * add abstract base class, which inherits ABC
    
    * add abstract method
    
    * implement Circle subclass
    
    * add __main__
    
    * add TODO objective
    
    * add ABC JSONify, w/ to_json abstract method
    
    * implement interface in concrete class, using mult-inherit & ABCs
    
    * add file for __str__ and __repr__ (object representation)
    
    * tweak
    
    * add 'BEFORE' file
    
    * add 'AFTER' file
    
    * default author=None
    
    * add Chapter class
    
    * add get_page_ct()
    
    * return book page count
    
    * rm comments
    
    * add __eq__
    
    * add more equality
    
    * add __iter__ and __next__
    
    * fix error: use 'and' for compound comparison (not &)
    
    * add all comparisons
    
    * demonstrate sorting functionality
    
    * add starting template of attr access file
    
    * implement __getattribute__
    
    * add __setattr__
    
    * add __getattr__ for ad-hoc attr definition
    
    * init callable objects lesson
    
    * compete callable-object magic method __call__
    
    Co-authored-by: KMQC <kmqc@novonordisk.com>

---
+++ ch3-magic__methods__/__eq__.py
@@ -0,0 +1,96 @@
+class XYZ:
+    def __init__(self):
+        self.xyz = 'XYZ'
+
+    def __eq__(self, other):
+        # must be of same class
+        if not isinstance(other, type(self)):
+            raise ValueError("Can't compare Node to non-Node type")
+        return self.xyz == other.xyz
+
+
+err = "Can't compare Node to non-Node type"
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return str(self.data)
+
+    def __eq__(self, other):
+        # must be of same class
+        if not isinstance(other, Node):
+            raise ValueError(err)
+
+        return (self.data == other.data) and (self.next == other.next)
+
+    # greater than
+    def __gt__(self, other):
+        if not isinstance(other, Node):
+            raise ValueError(err)
+        return self.data > other.data
+
+    # greater than or equal to
+    def __ge__(self, other):
+        if not isinstance(other, Node):
+            raise ValueError(err)
+        return self.data >= other.data
+
+    # less than
+    def __lt__(self, other):
+        if not isinstance(other, Node):
+            raise ValueError(err)
+        return self.data < other.data
+
+    # less than or equal to
+    def __le__(self, other):
+        if not isinstance(other, Node):
+            raise ValueError(err)
+        return self.data <= other.data
+
+
+# TODO
+# class LinkedList:
+#     def __init__(self, node=None):
+#         self.head = node
+#         self.length = 0 if node is None else 1
+
+
+if __name__ == "__main__":
+    c = Node('c')
+    c2 = Node('c')
+    print(c == c2)
+
+    b = Node('b', c)
+    print(c == b)
+
+    a = Node('a', b)
+    print(a, end=' '); print(b, end=' '); print(c)
+
+    xyz = XYZ()
+    xyz2 = XYZ()
+    print(xyz == xyz2)
+    try:
+        print(a == xyz)  # same as: >>> print(xyz == a)
+    except ValueError as e:
+        print("An error was expected; an error has occurred.")
+    
+    print("node equality:")
+    a2 = Node('a', b)
+    print(a == a2)
+
+    print("comparisons")
+    print(a > b, end=', '); print(b > a)
+    print(a >= b, end=', '); print(b >= a)
+    print(a < b, end=', '); print(b < a)
+    print(a <= b, end=', '); print(b <= a)
+
+    # not equal to
+    print(a2 != a)
+
+    nodes = [c, a2, b, c2, a]
+    nodes.sort()
+    print([node.data for node in nodes])  # equivalency
+    print([str(node) for node in nodes])  # equivalency
---
+++ ch3-magic__methods__/callable_objects.py
@@ -0,0 +1,30 @@
+class Book:
+    def __init__(self, title, author, price):
+        super().__init__()
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, costs {self.price}"
+
+    def __call__(self, title, author, price):
+        # The __call__ method can be used to call the object like a function
+        # This method will be invoked when I call my object. So it gets a copy of the object
+        self.title = title
+        self.author = author
+        self.price = price
+    # HOW THIS IS HELPFUL:
+    #  that's one of the benefits of this technique is if you have objects whose attributes change frequently, 
+    # or are often modified together, this can result in more compact code that's also easier to read.
+
+
+if __name__ == "__main__":    
+    b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
+    b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+    # call the object as if it were a function
+    # to change the values of the object's attributes
+    print(b1)
+    b1("Anna Karenina", "Leo Tolstoy", 49.95)
+    print(b1)
