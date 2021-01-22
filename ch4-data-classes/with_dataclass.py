6f8b7a1bd8ac7b9781f073c6ae66212d1b5004a0
Mark Chesney <mark.chesney@gmail.com>
2021-01-21 18:23:23 -0800

    Linkedin learning (#6)
    * add interface.py
    
    * add imports: ABC, abstractmethod
    
    * add abstract base class, which inherits ABC
    
    * add abstract method
    
    * implement Circle subclass
    
    * add __main__
    
    * add TODO objective
    
    * add ABC JSONify, w/ to_json abstract method
    
    * implement interface in concrete class, using mult-inherit & ABCs
    
    * add file for __str__ and __repr__ (object representation)
    
    * tweak
    
    * add 'BEFORE' file
    
    * add 'AFTER' file
    
    * default author=None
    
    * add Chapter class
    
    * add get_page_ct()
    
    * return book page count
    
    * rm comments
    
    * add __eq__
    
    * add more equality
    
    * add __iter__ and __next__
    
    * fix error: use 'and' for compound comparison (not &)
    
    * add all comparisons
    
    * demonstrate sorting functionality
    
    * add starting template of attr access file
    
    * implement __getattribute__
    
    * add __setattr__
    
    * add __getattr__ for ad-hoc attr definition
    
    * init callable objects lesson
    
    * compete callable-object magic method __call__
    
    * add Polymorphism notes to README
    
    * rename ch1 folder
    
    * move file
    
    * add comment
    
    * add data class (@dataclass)
    
    * add __main__
    
    * add __post_init__ files
    
    Co-authored-by: KMQC <kmqc@novonordisk.com>

---
+++ ch4-data-classes/with_dataclass.py
@@ -0,0 +1,52 @@
+# Python 3.7 introduces the start of Data Classes
+# used mostly to hold information
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+    title: str
+    author: str
+    pages: int
+    price: float
+
+    # You can define methods in a dataclass like any other
+    def bookinfo(self):
+        return f"{self.title}, by {self.author}"
+
+
+# ==========================================
+# Data Classes
+# ==========================================
+# @dataclass decorator implements __init__
+#  to initialize each attribute on the object instance.
+#
+# note: type hints required for this to work.
+#  however, the types are not enforced.
+#
+# What else Data Classes do:
+#   - __eq__ and __repr__ are automatically implemented
+
+
+if __name__ == "__main__":
+    # create some instances
+    b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+    b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+    b3 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+
+    # access fields
+    print(b1.title)
+    print(b2.author)
+
+    # print the book itself - dataclasses implement __repr__
+    # implementation of the __repr__ function
+    print(b1)
+    repr(b1)
+
+    # comparing two dataclasses - they implement __eq__
+    print(b1 == b2)
+    print(b2 == b3)
+
+    # change some fields, call a regular class method
+    b1.title = "Anna Karenina"
+    b1.pages = 864
+    print(b1.bookinfo())
